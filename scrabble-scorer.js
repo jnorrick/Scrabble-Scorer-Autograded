3e2cdee80e903e489ecb67cc8c920f613501d683
Jessica Orrick <jessica.orrick@outlook.com>
2023-10-04 23:55:20 -0500

    completed task 1

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -3,38 +3,38 @@
 const input = require("readline-sync");
 
 const oldPointStructure = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z']
+	1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+	2: ['D', 'G'],
+	3: ['B', 'C', 'M', 'P'],
+	4: ['F', 'H', 'V', 'W', 'Y'],
+	5: ['K'],
+	8: ['J', 'X'],
+	10: ['Q', 'Z']
 };
 
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
- 
 	for (let i = 0; i < word.length; i++) {
- 
-	  for (const pointValue in oldPointStructure) {
- 
-		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
-		 }
- 
-	  }
+
+		for (const pointValue in oldPointStructure) {
+
+			if (oldPointStructure[pointValue].includes(word[i])) {
+				letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+			}
+		}
 	}
 	return letterPoints;
- }
+}
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
-};
+	let word = input.question("Let's play some scrabble! \n\nEnter a word to score: ");
+	return word;
+}
+
 
 let simpleScorer;
 
@@ -51,21 +51,22 @@ function transform() {};
 let newPointStructure;
 
 function runProgram() {
-   initialPrompt();
-   
+	let promptResults = initialPrompt();
+	let results = oldScrabbleScorer(promptResults);
+	console.log(results);
 }
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
-   initialPrompt: initialPrompt,
-   transform: transform,
-   oldPointStructure: oldPointStructure,
-   simpleScorer: simpleScorer,
-   vowelBonusScorer: vowelBonusScorer,
-   scrabbleScorer: scrabbleScorer,
-   scoringAlgorithms: scoringAlgorithms,
-   newPointStructure: newPointStructure,
+	initialPrompt: initialPrompt,
+	transform: transform,
+	oldPointStructure: oldPointStructure,
+	simpleScorer: simpleScorer,
+	vowelBonusScorer: vowelBonusScorer,
+	scrabbleScorer: scrabbleScorer,
+	scoringAlgorithms: scoringAlgorithms,
+	newPointStructure: newPointStructure,
 	runProgram: runProgram,
 	scorerPrompt: scorerPrompt
 };
